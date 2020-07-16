92f133ba48b1f96c79309d59ec2fb0eb1f40ca48
farisaziz12 <farisaziz12@gmail.com>
2020-07-16 11:06:11 +0200

    flashcards now being saved to localstorage on creation and fetched on refresh

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import FlashCardCreate from "./Components/FlashCardCreate";
 import FlashCard from "./Components/FlashCard";
@@ -7,26 +7,55 @@ function App() {
   const [flashcards, setFlashcards] = useState([]);
   const [newCardQuestion, setNewCardQuestion] = useState("");
   const [newCardAnswer, setNewCardAnswer] = useState("");
+  const [testMode, setTestMode] = useState(false);
+
+  useEffect(() => {
+    const storedFlashcards = localStorage.getItem("flashcards");
+    if (storedFlashcards) {
+      setFlashcards(storedFlashcards);
+    }
+  }, []);
 
   const saveNewCard = () => {
-    const flashcard = { question: newCardQuestion, answer: newCardAnswer };
+    const id = flashcards.length + 1;
+    const flashcard = {
+      id: id,
+      question: newCardQuestion,
+      answer: newCardAnswer,
+    };
     setFlashcards([...flashcards, flashcard]);
     setNewCardQuestion("");
     setNewCardAnswer("");
+
+    localStorage.setItem(
+      "flashcards",
+      JSON.stringify([...flashcards, flashcard])
+    );
   };
 
   return (
     <>
       <h1>Flash Cards</h1>
-      <FlashCardCreate
-        question={newCardQuestion}
-        setQuestion={setNewCardQuestion}
-        answer={newCardAnswer}
-        setAnswer={setNewCardAnswer}
-        saveNewCard={saveNewCard}
-      />
-      {newCardQuestion && (
-        <FlashCard question={newCardQuestion} answer={newCardAnswer} />
+      <button onClick={() => setTestMode(!testMode)}>
+        {testMode ? "Create" : "Test"}
+      </button>
+      {testMode ? (
+        <>
+          <h2>Flascards: {flashcards.length}</h2>
+        </>
+      ) : (
+        <>
+          <FlashCardCreate
+            question={newCardQuestion}
+            setQuestion={setNewCardQuestion}
+            answer={newCardAnswer}
+            setAnswer={setNewCardAnswer}
+            saveNewCard={saveNewCard}
+          />
+          {newCardQuestion && (
+            <FlashCard question={newCardQuestion} answer={newCardAnswer} />
+          )}
+        </>
       )}
     </>
   );
