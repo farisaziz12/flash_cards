ef3dbdede21778d39b3f768b1b5548f93099ff50
farisaziz12 <farisaziz12@gmail.com>
2020-07-16 18:04:19 +0200

    comments added

--- src/App.js
+++ src/App.js
@@ -14,23 +14,28 @@ function App() {
   const [cardsFinished, setCardsFinished] = useState(false);
 
   useEffect(() => {
+    // Fetching stored flashcards from localstorage
     const storedFlashcards = localStorage.getItem("flashcards");
+    // If stored flashcards exist the JSON is parsed into a JS object and is set in state
     if (storedFlashcards) {
       setFlashcards(JSON.parse(storedFlashcards));
     }
   }, []);
 
   const saveNewCard = () => {
+    // An ID is alloted to a new card, which is + 1 greater than the length of the existing cards array
     const id = flashcards.length + 1;
     const flashcard = {
       id: id,
       question: newCardQuestion,
       answer: newCardAnswer,
     };
+    // The new card is added to the array of existing cards and the input fields are reset
     setFlashcards([...flashcards, flashcard]);
     setNewCardQuestion("");
     setNewCardAnswer("");
 
+    // Localstorage is updated with the latest collection of flashcards by stringifying the array of flashcards
     localStorage.setItem(
       "flashcards",
       JSON.stringify([...flashcards, flashcard])
@@ -38,7 +43,9 @@ function App() {
   };
 
   const launchTestMode = () => {
+    // Setting testMode state to true to conditionally render the test view
     setTestMode(true);
+    // If flashcards exist then set the current card to a random flashcard
     if (flashcards[0]) {
       const card = flashcards[Math.floor(Math.random() * flashcards.length)];
       setCurrentCard(card);
@@ -46,15 +53,18 @@ function App() {
   };
 
   const handleFlashCardCorrect = (correct) => {
+    // Finding leftover cards via filtering currently answered card and list of completed cards
     const completedCards = cardsRight.concat(cardsWrong);
     const leftOverCards = flashcards.filter(
       (flashcard) =>
         !completedCards.includes(flashcard.id) &&
         flashcard.id !== currentCard.id
     );
+    // Finding next card randomly using leftover cards
     const nextCard =
       leftOverCards[Math.floor(Math.random() * leftOverCards.length)];
 
+    // Seperating correct and incorrect answers by flashcard ID and setting next card in state
     if (correct) {
       setCardsRight([...cardsRight, currentCard.id]);
       if (nextCard) {
@@ -67,7 +77,9 @@ function App() {
       }
     }
     if (!nextCard) {
+      // Setting finished state to true to end card shuffling
       setCardsFinished(true);
+      // Alerting user of score
       window.alert(
         `You got ${
           correct ? cardsRight.length + 1 : cardsRight.length
